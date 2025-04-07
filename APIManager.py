a2a91cad3892c5510c8d448770a04c2b72e80ee1
il798li <92280179+il798li@users.noreply.github.com>
2025-04-07 11:48:24 -0700

    Update APIManager.py
    moved API key management to within the getBazaarData function

--- APIManager.py
+++ APIManager.py
@@ -1,9 +1,10 @@
 import requests
 from os import getenv
 
-apiKey: str = getenv("API_KEY")
-url: str = f"https://api.hypixel.net/skyblock/bazaar?key={apiKey}"
-
 def getBazaarData() -> dict:
+    apiKey: str = getenv("API_KEY")
+    if apiKey == None:
+        raise Exception("You have not provided an API key!")
+    url: str = f"https://api.hypixel.net/skyblock/bazaar?key={apiKey}"
     response: requests.Response = requests.get(url)
     return response.json()
