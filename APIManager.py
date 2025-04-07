a3541ed2aa6b81c06358e3db4eeb51cb3452921e
il798li <92280179+il798li@users.noreply.github.com>
2025-04-07 12:00:38 -0700

    removed use of API keys

--- APIManager.py
+++ APIManager.py
@@ -1,10 +1,6 @@
 import requests
-from os import getenv
 
 def getBazaarData() -> dict:
-    apiKey: str = getenv("API_KEY")
-    if apiKey == None:
-        raise Exception("You have not provided an API key!")
-    url: str = f"https://api.hypixel.net/skyblock/bazaar?key={apiKey}"
+    url: str = f"https://api.hypixel.net/skyblock/bazaar"
     response: requests.Response = requests.get(url)
     return response.json()
